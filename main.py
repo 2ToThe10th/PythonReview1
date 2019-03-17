9444f26a3c17c64905d7beed44a809fa59f8006c
2ToThe10th <grib.a@phystech.edu>
2019-03-17 19:45:15 +0300

    Parser made

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-import sys
-import argparse
+import parser
 
-parser = argparse.ArgumentParser(description="encoder")
-parser.add_argument('type', action='store', type=str)
-
-if 'encode' in sys.argv:
-    parser.add_argument('--cipher', action='store', type=str, required=True)
-    parser.add_argument('--key', action='store', type=str, required=True)
-    parser.add_argument('--input-file', action='store', type=str)
-    parser.add_argument('--output-file', action='store', type=str)
-
-elif 'decode' in sys.argv:
-    parser.add_argument('--cipher', action='store', type=str, required=True)
-    parser.add_argument('--key', action='store', type=str, required=True)
-    parser.add_argument('--input-file', action='store', type=str)
-    parser.add_argument('--output-file', action='store', type=str)
-
-elif 'train' in sys.argv:
-    parser.add_argument('--text-file', action='store', type=str)
-    parser.add_argument('--model-file', action='store', type=str, required=True)
-
-elif 'hack' in sys.argv:
-    parser.add_argument('--input-file', action='store', type=str)
-    parser.add_argument('--output-file', action='store', type=str)
-    parser.add_argument('--model-file', action='store', type=str, required=True)
-else:
-    raise ValueError('argument "type" is required')
-
-print(parser.parse_args())
+print(parser.Parse())
---
+++ parser.py
@@ -0,0 +1,37 @@
+def Parse():
+
+    import sys
+    import argparse
+
+    parser = argparse.ArgumentParser(description="encoder")
+    parser.add_argument('type', action='store', type=str)
+
+    type_of_cipher = ('caesar', 'vigenere', 'vernam')
+
+    if 'encode' == sys.argv[1]:
+        parser.add_argument('--cipher', action='store', type=str, required=True, choices=type_of_cipher)
+        parser.add_argument('--key', action='store', type=str, required=True)
+        parser.add_argument('--input-file', action='store', type=str)
+        parser.add_argument('--output-file', action='store', type=str)
+
+    elif 'decode' == sys.argv[1]:
+        parser.add_argument('--cipher', action='store', type=str, required=True, choices=type_of_cipher)
+        parser.add_argument('--key', action='store', type=str, required=True)
+        parser.add_argument('--input-file', action='store', type=str)
+        parser.add_argument('--output-file', action='store', type=str)
+
+    elif 'train' == sys.argv[1]:
+        parser.add_argument('--text-file', action='store', type=str)
+        parser.add_argument('--model-file', action='store', type=str, required=True)
+
+    elif 'hack' == sys.argv[1]:
+        parser.add_argument('--cipher', action='store', type=str, required=True, choices=type_of_cipher)
+        parser.add_argument('--input-file', action='store', type=str)
+        parser.add_argument('--output-file', action='store', type=str)
+        parser.add_argument('--model-file', action='store', type=str, required=True)
+
+    else:
+        raise ValueError('argument "type" is required')
+
+    return parser.parse_args()
+
